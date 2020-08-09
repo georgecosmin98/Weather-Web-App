780929e9394da1d5a3a85814800b569137d5a648
georgecosmin98 <ilincageorgecomsin@yahoo.com>
2020-08-09 17:27:18 +0300

    Auto update weather icon

--- script.js
+++ script.js
@@ -2,40 +2,58 @@ function weatherBalloon() {
     let long;
     let lat;
 
-    if(navigator.geolocation){
+    if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
             long = position.coords.longitude;
             lat = position.coords.latitude;
 
-    var key = 'f8eb06bf60edeef10a250fbf59f3d997';
+            var key = 'f8eb06bf60edeef10a250fbf59f3d997';
 
-    fetch('https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + '&appid=' + key)
-        .then(function(resp) { return resp.json() }) // Convert data to json
-        .then(function(data) {
-            console.log(data);
-            drawWeather(data);
-            drawIconWeather(data);
+            fetch('https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + '&appid=' + key)
+                .then(function (resp) {
+                    return resp.json()
+                }) // Convert data to json
+                .then(function (data) {
+                    console.log(data);
+                    drawWeather(data);
+                    drawIconWeather(data);
+                })
+                .catch(function () {
+                    // catch any errors
+                });
         })
-        .catch(function() {
-            // catch any errors
-        });
-})}}
+    }
+}
 
-window.onload = function() {
-    weatherBalloon( 6167865 );
+window.onload = function () {
+    weatherBalloon(6167865);
 }
 
 function drawWeather(data) {
-    var celcius = Math.round(parseFloat(data.main.temp)-273.15);
-    var fahrenheit = Math.round(((parseFloat(data.main.temp)-273.15)*1.8)+32);
+    var celcius = Math.round(parseFloat(data.main.temp) - 273.15);
+    var fahrenheit = Math.round(((parseFloat(data.main.temp) - 273.15) * 1.8) + 32);
 
     document.getElementById('descriptionId').innerHTML = data.weather[0].description;
     document.getElementById('degreeId').innerHTML = celcius + '&deg;';
     document.getElementById('timezoneId').innerHTML = data.name;
 }
 
-function drawIconWeather(data){
+function drawIconWeather(data) {
     var icons = new Skycons({"color": "orange"});
-    icons.set("icon", Skycons.RAIN);
+    switch (data.weather[0].main) {
+        case "Clouds":
+            icons.set("icon", Skycons.CLOUDY);
+            break;
+        case "Clear":
+            icons.set("icon", Skycons.CLEAR_DAY);
+            break;
+        case "Snow":
+            icons.set("icon", Skycons.SNOW);
+            break;
+        case "Rain":
+            icons.set("icon", Skycons.RAIN);
+            break;
+    }
+
     icons.play();
 }
